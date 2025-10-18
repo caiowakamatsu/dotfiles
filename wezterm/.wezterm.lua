f1888d9c0f08f88e3fc2d4d94911fc0517572bf5
caiowakamatsu <caiodcw@gmail.com>
2025-10-18 06:26:48 -0300

    updates

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require 'wezterm'
+return {
+  font = wezterm.font_with_fallback({"JetBrains Mono", "Iosevka Term"}),
+  font_size = 12.0,
+  window_background_opacity = 0.90,
+  macos_window_background_blur = 20,
+  color_scheme = "Builtin Dark",
+	window_decorations = "RESIZE",
+	enable_tab_bar = false,
+	use_resize_increments = false,
+  colors = {
+    background = "#0C0F14",
+    foreground = "#E0E1DD",
+    cursor_bg = "#6EE2FF",
+    cursor_fg = "#0C0F14",
+  },
+}
